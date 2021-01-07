47305873716c177c54857ce900e320fa5f51098e
LaurenJeffrey <lauren.jeffrey@ocsbstudent.ca>
2021-01-07 15:54:34 +0000

    js fixed

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-let diameter;
-let circumference;
-let area;
+let diameter
+let circumference
+let area
 
-document.getElementById("findarea").addEventListener("click", areafunction)
-function areafunction(){
-diameter = document.getElementById("diametera").value
-diameter = +diameter;
-let radius = diameter/2;
-let π = 3.14;
-area = π*radius*radius;
-alert(area)
+document.getElementById('findarea').addEventListener('click', areafunction)
+function areafunction () {
+  diameter = document.getElementById('diametera').value
+  diameter = +diameter
+  const radius = diameter / 2
+  const π = 3.14
+  area = π * radius * radius
+  alert(area)
 }
 document.getElementById('diameterc').addEventListener('click', circumferencefunction)
-function circumferencefunction(){
-diameter = document.getElementById("diameterc").value
-diameter = +diameter;
-let radius = diameter/2;
-let π = 3.14;
-circumference = π*2*radius;
-alert(circumference)
+function circumferencefunction () {
+  diameter = document.getElementById('diameterc').value
+  diameter = +diameter
+  const radius = diameter / 2
+  const π = 3.14
+  circumference = π * 2 * radius
+  alert(circumference)
 }
