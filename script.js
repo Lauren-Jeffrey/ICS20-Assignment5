026499518d88825d0f95a8bf4d316bff17153f9e
LaurenJeffrey <lauren.jeffrey@ocsbstudent.ca>
2021-01-07 00:18:55 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let diameter;
+let circumference;
+let area;
+
+document.getElementById("findarea").addEventListener("click", areafunction)
+function areafunction(){
+diameter = document.getElementById("diametera").value
+diameter = +diameter;
+let radius = diameter/2;
+let π = 3.14;
+area = π*radius*radius;
+alert(area)
+}
+document.getElementById('diameterc').addEventListener('click', circumferencefunction)
+function circumferencefunction(){
+diameter = document.getElementById("diameterc").value
+diameter = +diameter;
+let radius = diameter/2;
+let π = 3.14;
+circumference = 2*π*radius;
+alert(circumference)
+}
